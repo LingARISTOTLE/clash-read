5d90ec47908faf8f6fed8a816324a2427792468b
yangling <2397328561@qq.com>
2025-08-23 17:45:46 +0800

    docs: 添加 Clash 数据流向图并优化 HTTP 代理代码
    - 新增 geoip.plantuml 文件，绘制 Clash 数据流向图
    -优化 HTTP 代理代码，增加注释和文档说明
    - 改进 HttpAdapter 结构和方法，实现 ServerAdapter 接口
    - 添加分块传输编码处理逻辑

--- proxy/http/http.go
+++ proxy/http/http.go
@@ -1,3 +1,5 @@
+// Package http 实现了HTTP代理功能
+// 处理HTTP和HTTPS CONNECT请求，将客户端请求转发到目标服务器
 package http
 
 import (
@@ -9,69 +11,107 @@ import (
 	C "../../constant"
 )
 
+// HttpAdapter 是HTTP代理适配器
+// 实现了ServerAdapter接口，用于处理HTTP连接请求
 type HttpAdapter struct {
-	addr *C.Addr
-	r    *http.Request
-	w    http.ResponseWriter
-	done chan struct{}
+	addr *C.Addr             // 目标地址信息
+	r    *http.Request       // 原始HTTP请求
+	w    http.ResponseWriter // HTTP响应写入器
+	done chan struct{}       // 完成通知通道
 }
 
+// Close 关闭HTTP适配器连接
+// 通过向done通道发送信号通知连接处理完成
+// 这个方法实现了ServerAdapter接口
 func (h *HttpAdapter) Close() {
 	h.done <- struct{}{}
 }
 
+// Addr 返回目标地址信息
+// 这个方法实现了ServerAdapter接口
 func (h *HttpAdapter) Addr() *C.Addr {
 	return h.addr
 }
 
+// Connect 建立连接并转发HTTP请求到目标服务器
+// proxy: 代理适配器，用于建立到目标服务器的连接
+// 这个方法实现了ServerAdapter接口
 func (h *HttpAdapter) Connect(proxy C.ProxyAdapter) {
+	// 创建HTTP传输对象，用于转发请求到目标服务器
 	req := http.Transport{
+		// 自定义Dial函数，使用代理适配器建立连接
 		Dial: func(string, string) (net.Conn, error) {
 			return proxy.Conn(), nil
 		},
-		// from http.DefaultTransport
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
+		// 以下参数来自http.DefaultTransport的默认配置
+		MaxIdleConns:          100,              // 最大空闲连接数
+		IdleConnTimeout:       90 * time.Second, // 空闲连接超时时间
+		ExpectContinueTimeout: 1 * time.Second,  // Expect请求超时时间
 	}
+
+	// 使用传输对象发送HTTP请求到目标服务器
 	resp, err := req.RoundTrip(h.r)
 	if err != nil {
+		// 如果请求失败，直接返回
 		return
 	}
+	// 函数结束时关闭响应体
 	defer resp.Body.Close()
 
+	// 将目标服务器的响应头复制到客户端响应中
 	header := h.w.Header()
 	for k, vv := range resp.Header {
 		for _, v := range vv {
 			header.Add(k, v)
 		}
 	}
+
+	// 设置响应状态码并写入响应头
 	h.w.WriteHeader(resp.StatusCode)
+
+	// 根据传输编码类型选择合适的写入器
 	var writer io.Writer = h.w
 	if len(resp.TransferEncoding) > 0 && resp.TransferEncoding[0] == "chunked" {
+		// 如果是分块传输编码，使用ChunkWriter处理
 		writer = ChunkWriter{Writer: h.w}
 	}
+
+	// 将目标服务器的响应体数据复制到客户端
 	io.Copy(writer, resp.Body)
 }
 
+// ChunkWriter 是分块传输编码的写入器
+// 用于处理HTTP分块传输编码的响应数据
 type ChunkWriter struct {
-	io.Writer
+	io.Writer // 嵌入Writer接口，获得Write方法
 }
 
+// Write 实现io.Writer接口的Write方法
+// 写入数据并在成功后刷新缓冲区
 func (cw ChunkWriter) Write(b []byte) (int, error) {
+	// 写入数据
 	n, err := cw.Writer.Write(b)
 	if err == nil {
+		// 如果写入成功，刷新缓冲区确保数据立即发送
 		cw.Writer.(http.Flusher).Flush()
 	}
 	return n, err
 }
 
+// NewHttp 创建一个新的HTTP适配器实例
+// host: 目标主机地址
+// w: HTTP响应写入器
+// r: 原始HTTP请求
+// 返回: HTTP适配器实例和完成通知通道
 func NewHttp(host string, w http.ResponseWriter, r *http.Request) (*HttpAdapter, chan struct{}) {
+	// 创建完成通知通道
 	done := make(chan struct{})
+
+	// 返回配置好的HTTP适配器实例
 	return &HttpAdapter{
-		addr: parseHttpAddr(host),
-		r:    r,
-		w:    w,
-		done: done,
+		addr: parseHttpAddr(host), // 解析目标地址
+		r:    r,                   // 保存原始请求
+		w:    w,                   // 保存响应写入器
+		done: done,                // 保存完成通道
 	}, done
 }
--- proxy/http/server.go
+++ proxy/http/server.go
@@ -1,3 +1,5 @@
+// Package http 实现了HTTP代理服务端功能
+// 包括HTTP服务器启动、请求处理和HTTPS隧道建立等功能
 package http
 
 import (
@@ -13,74 +15,125 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// 全局变量，获取Tunnel单例实例
+// 用于将HTTP请求转发到Tunnel模块进行处理
 var (
 	tun = tunnel.GetInstance()
 )
 
+// NewHttpProxy 创建并启动HTTP代理服务器
+// port: 监听端口号
 func NewHttpProxy(port string) {
+	// 创建HTTP服务器实例
 	server := &http.Server{
+		// 设置服务器监听地址
 		Addr: fmt.Sprintf(":%s", port),
+		// 设置请求处理器
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// 根据请求方法进行不同处理
 			if r.Method == http.MethodConnect {
+				// HTTPS CONNECT请求处理
 				handleTunneling(w, r)
 			} else {
+				// 普通HTTP请求处理
 				handleHTTP(w, r)
 			}
 		}),
 	}
+
+	// 记录日志信息
 	log.Infof("HTTP proxy :%s", port)
+
+	// 启动HTTP服务器并开始监听
 	server.ListenAndServe()
 }
 
+// handleHTTP 处理普通的HTTP请求
+// w: HTTP响应写入器
+// r: HTTP请求
 func handleHTTP(w http.ResponseWriter, r *http.Request) {
+	// 获取目标地址
 	addr := r.Host
+
+	// 如果地址中没有端口号，则添加默认的HTTP端口80
 	// padding default port
 	if !strings.Contains(addr, ":") {
 		addr += ":80"
 	}
+
+	// 创建HTTP适配器实例
 	req, done := NewHttp(addr, w, r)
+
+	// 将请求添加到Tunnel处理队列中
 	tun.Add(req)
+
+	// 等待请求处理完成
 	<-done
 }
 
+// handleTunneling 处理HTTPS的CONNECT请求，建立隧道连接
+// w: HTTP响应写入器
+// r: HTTP请求
 func handleTunneling(w http.ResponseWriter, r *http.Request) {
+	// 检查响应写入器是否实现了Hijacker接口
+	// Hijacker接口允许接管底层连接
 	hijacker, ok := w.(http.Hijacker)
 	if !ok {
+		// 如果不支持连接劫持，则直接返回
 		return
 	}
+
+	// 劫持HTTP连接，获取底层TCP连接
 	conn, _, err := hijacker.Hijack()
 	if err != nil {
+		// 如果劫持连接失败，则直接返回
 		return
 	}
-	// w.WriteHeader(http.StatusOK) doesn't works in Safari
+
+	// 向客户端发送连接建立成功的响应
+	// 注意：不能使用w.WriteHeader(http.StatusOK)，因为在Safari浏览器中不工作
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	// 创建HTTPS适配器并添加到Tunnel处理队列
 	tun.Add(NewHttps(r.Host, conn))
 }
 
+// parseHttpAddr 解析HTTP目标地址并创建Addr结构体
+// target: 目标地址字符串，格式为"host:port"
+// 返回: 解析后的地址结构体
 func parseHttpAddr(target string) *C.Addr {
+	// 分割主机名和端口号
 	host, port, _ := net.SplitHostPort(target)
+
+	// 解析IP地址
 	ipAddr, err := net.ResolveIPAddr("ip", host)
 	var resolveIP *net.IP
 	if err == nil {
+		// 如果解析成功，保存解析后的IP地址
 		resolveIP = &ipAddr.IP
 	}
 
+	// 确定地址类型
 	var addType int
 	ip := net.ParseIP(host)
 	switch {
 	case ip == nil:
+		// 如果不是IP地址，则为域名类型
 		addType = socks.AtypDomainName
 	case ip.To4() == nil:
+		// 如果不是IPv4地址，则为IPv6地址类型
 		addType = socks.AtypIPv6
 	default:
+		// 默认为IPv4地址类型
 		addType = socks.AtypIPv4
 	}
 
+	// 创建并返回地址结构体
 	return &C.Addr{
-		NetWork:  C.TCP,
-		AddrType: addType,
-		Host:     host,
-		IP:       resolveIP,
-		Port:     port,
+		NetWork:  C.TCP,     // 网络类型为TCP
+		AddrType: addType,   // 地址类型（域名、IPv4或IPv6）
+		Host:     host,      // 主机名
+		IP:       resolveIP, // 解析后的IP地址
+		Port:     port,      // 端口号
 	}
 }

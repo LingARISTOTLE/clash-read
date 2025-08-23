1a2aa99ea69b45b6102dc3138aff4d1bc52f598d
yangling <2397328561@qq.com>
2025-08-23 16:11:37 +0800

    feat: 添加基本功能
    - 新增配置管理模块
    - 实现直连和拒绝连接的适配器
    - 添加域名后缀、域名关键字、IP CIDR、GeoIP等规则类型
    - 实现HTTP和SOCKS代理功能- 添加外部控制器支持
    - 实现流量统计和日志记录功能
    - 添加单元测试

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	C "github.com/fossabot/clash/constant"
+	"github.com/fossabot/clash/hub"
+	"github.com/fossabot/clash/proxy/http"
+	"github.com/fossabot/clash/proxy/socks"
+	"github.com/fossabot/clash/tunnel"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	cfg, err := C.GetConfig()
+	if err != nil {
+		log.Fatalf("Read config error: %s", err.Error())
+	}
+
+	port, socksPort := C.DefalutHTTPPort, C.DefalutSOCKSPort
+	section := cfg.Section("General")
+	if key, err := section.GetKey("port"); err == nil {
+		port = key.Value()
+	}
+
+	if key, err := section.GetKey("socks-port"); err == nil {
+		socksPort = key.Value()
+	}
+
+	err = tunnel.GetInstance().UpdateConfig()
+	if err != nil {
+		log.Fatalf("Parse config error: %s", err.Error())
+	}
+
+	go http.NewHttpProxy(port)
+	go socks.NewSocksProxy(socksPort)
+
+	// Hub
+	if key, err := section.GetKey("external-controller"); err == nil {
+		go hub.NewHub(key.Value())
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+}
